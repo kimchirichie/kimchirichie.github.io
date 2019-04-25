2fdbfcad702021346099aa707cb84d94bfda59f6
Rick Sin <sin@roofr.com>
2019-04-24 21:07:47 -0700

    initial commit

---
+++ richardsin/javascript/portfolio.js
@@ -0,0 +1,23 @@
+var App = angular.module('portfolioApp', []);
+
+App.controller("portfolioController", function ($http){
+
+	var vm = this;
+	var formId = 1;
+	vm.user = {};
+
+	vm.getform = function (){
+		// var link = "/db/webform?id="+formId;
+		var link = "/db/webform?id=1";
+		$http.get(link).then(function(res){
+			vm.user = res.data;
+			vm.user.domains = JSON.parse(vm.user.domains);
+			vm.user.contents = JSON.parse(vm.user.contents);
+			console.log(res.data);
+		},function(err){
+			console.log(err);
+		});
+	}
+	
+	vm.getform();
+});
